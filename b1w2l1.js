598eca7979780f7a30b4ba1f349f66985dbc3005
Levi Hoevers <99065078@mydavinci.nl>
2020-09-08 11:32:40 +0200

    lab 1 toegevoegd

---
+++ b1w2l1.js
@@ -0,0 +1,10 @@
+// begin hier je JavaScript commando
+
+//Levi Hoevers
+//99065078
+//softwaredeveloper
+
+var Fname = prompt("wat is je voornaam?"); // input bij prompt word opgeslagen bij Fname bij, Sname en age gaat het precies hetzelfde.
+var Sname = prompt("wat is je achternaam?");
+var age = prompt("wat is je leeftijd?");
+document.write(Fname + ' ' + Sname + ' ' + age);//schrijft de input op het scherm
