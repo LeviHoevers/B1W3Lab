aca25d20d101f25a504dee075059c68523b787e5
Levi Hoevers <99065078@mydavinci.nl>
2020-09-08 11:57:19 +0200

    lab 2 toegevoegd

---
+++ lab2/lab2.js
@@ -0,0 +1,8 @@
+//Levi hoevers
+//99065078
+// softwaredeveloper
+
+var Fname = prompt('wat is uw voornaam') //info word opgeslagen Fname Sname of age
+var Sname = prompt('wat is uw achternaam')
+var age = prompt('wat is uw leeftijd')
+document.write( Fname + ' ' + Sname + ' ' + age) // de info die in een van de 3 variabele is opgeslagen word door document.write op het scherm gezet
