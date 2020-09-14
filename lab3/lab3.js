e09e70f5147ee5a166253d521997fe7a0dbaced1
Levi Hoevers <99065078@mydavinci.nl>
2020-09-14 10:50:42 +0200

    lab 3
    tafel 3 toegevoegd

--- lab3/lab3.js
+++ lab3/lab3.js
@@ -15,18 +15,36 @@ const EIGHT = 8;
 const NINE = 9;
 const TEN = 10;
 
-var AnswerOne = ONE * start
-var AnswerTwo = TWO * start
-var AnswerThree = THREE * start
-var AnswerFour = FOUR * start
-var AnswerFive = FIVE * start
-var AnswerSix = SIX * start
-var AnswerSeven = SEVEN * start
-var AnswerEight = EIGHT * start
-var AnswerNine = NINE * start
-var AnswerTen = TEN * start
+var AnswerOne = ONE * start ;
+var AnswerTwo = TWO * start ;
+var AnswerThree = THREE * start ;
+var AnswerFour = FOUR * start ;
+var AnswerFive = FIVE * start ;
+var AnswerSix = SIX * start;
+var AnswerSeven = SEVEN * start ;
+var AnswerEight = EIGHT * start ;
+var AnswerNine = NINE * start ;
+var AnswerTen = TEN * start ;
 
-document.write(AnswerOne + "<br>"  + AnswerTwo+ "<br>" + AnswerThree + "<br>"  + AnswerFour + "<br>" + AnswerFive + "<br>" + AnswerSix + "<br>" + AnswerSeven + "<br>"
-+ AnswerEight + "<br>"  + AnswerNine + "<br>"  + AnswerTen)
+
+var begin = 3;
+
+var AnswerOneA = ONE * begin ;
+var AnswerTwoA = TWO * begin ;
+var AnswerThreeA = THREE * begin ;
+var AnswerFourA = FOUR * begin ; 
+var AnswerFiveA = FIVE * begin ;
+var AnswerSixA = SIX * begin ;
+var AnswerSevenA = SEVEN * begin ;
+var AnswerEightA = EIGHT * begin ; 
+var AnswerNineA = NINE * begin ;
+var AnswerTenA = TEN * begin ;
+
+
+document.write( "De tafel van 6" + "<br>" + AnswerOne + "<br>"  + AnswerTwo+ "<br>" + AnswerThree + "<br>"  + AnswerFour + "<br>" + AnswerFive + "<br>" + AnswerSix + "<br>" +
+AnswerSeven + "<br>"+ AnswerEight + "<br>"  + AnswerNine + "<br>"  + AnswerTen + "<br>" + "<br>" + "De tafel van 3" + "<br>" + AnswerOneA + "<br>"+ AnswerTwoA + "<br>"+ AnswerThreeA + "<br>" + 
+AnswerFourA + "<br>" + AnswerFiveA + "<br>" + AnswerSixA + "<br>" + AnswerSevenA + "<br>" + AnswerEightA + "<br>" + AnswerNineA + "<br>" + AnswerTenA)
+
+
 
 
